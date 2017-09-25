4fbce9b3a25c1feca48f9abdf390b7fe733dfe87
Drake Eidukas <eidukas2@illinois.edu>
2017-09-25 01:53:41 -0500

    Add Carousel, scroll detection

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,16 +1,44 @@
 var originalNavBarHeight = document.getElementById('nav-cloud-box').style.height;
 
+var currentPictureIndex = 0
+var pictureArray = [
+    'assets/rails.png',
+    'assets/amazon.png',
+    'assets/sodexo.png'
+]
+
+function changeCarouselPic(index) {
+    let carousel = document.getElementById('carousel-pic')
+    carousel.src = pictureArray[index]
+}
+
+document.getElementById('left-carousel-button').onclick = function () {
+    if (currentPictureIndex === 0) {
+        currentPictureIndex = pictureArray.length - 1
+    } else {
+        currentPictureIndex--;
+    }
+    changeCarouselPic(currentPictureIndex)
+}
+
+document.getElementById('right-carousel-button').onclick = function () {
+    if (currentPictureIndex === pictureArray.length - 1) {
+        currentPictureIndex = 0
+    } else {
+        currentPictureIndex++;
+    }
+    changeCarouselPic(currentPictureIndex)
+}
+
 var modal = document.getElementById('factory-modal-pic');
 modal.style.display = 'none'
 var factoryPic = document.getElementById('normal-factory-pic')
 
 factoryPic.onclick = function () {
     modal.style.display = 'block'
-    console.log('factorypic clicked')
 }
 
 window.onclick = function (event) {
-    console.log('window clicked')
     if (event.target == modal) {
         modal.style.display = 'none'
     }
@@ -21,9 +49,26 @@ document.body.onscroll = function (event) {
     changeSelectedByScroll(document.body.scrollTop)
 }
 
+function highlightLi (element) {
+    let selectables = document.getElementsByClassName('selectable')
+    Array.prototype.map.call(selectables, function (element) {
+        element.classList.remove('active')
+    })
+
+    element.classList.add('active')
+}
+
 function changeSelectedByScroll (scrolled) {
-    if (scrolled > 0)
     console.log(scrolled)
+    if (scrolled < 1450) {
+        highlightLi(document.getElementById('reviews-li'))
+    } else if (scrolled < 2200) {
+        highlightLi(document.getElementById('sponsors-li'))
+    } else if (scrolled < 2750) {
+        highlightLi(document.getElementById('video-li'))
+    } else {
+        highlightLi(document.getElementById('stay-in-contact-li'))
+    }
 }
 
 function resizeNavBar(shrink) {
@@ -36,7 +81,7 @@ function resizeNavBar(shrink) {
         
         // Shrink the width of the elements on the bottom of the nav bar to make room for logo
         Array.prototype.map.call(document.getElementsByClassName('selectable'), function (element) {
-            element.style.width = '30%'
+            element.style.width = '22%'
         })
 
         // Insert logo on bottom of nav bar
@@ -52,7 +97,7 @@ function resizeNavBar(shrink) {
 
         // Expand the width of the elements on the bottom of the nav bar-- the logo is no longer present
         Array.prototype.map.call(document.getElementsByClassName('selectable'), function (element) {
-            element.style.width = '33%'
+            element.style.width = '25%'
         })
     
         // Remove logo on bottom of nav bar
@@ -76,8 +121,3 @@ function setOnmouseoverNavBar () {
 }
 
 setOnmouseoverNavBar()
-
-
-
-console.log('mem')
-
